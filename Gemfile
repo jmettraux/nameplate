718c2dc680f4e97b9dee03f6cb9c85b8402442fb
John Mettraux <jmettraux@gmail.com>
2010-08-16 10:48:23 +0900

    initial version

---
+++ Gemfile
@@ -0,0 +1,9 @@
+
+source "http://rubygems.org"
+
+gem 'thin'
+gem 'haml'
+gem 'sinatra'
+gem 'yajl-ruby'
+gem 'rufus-json'
+
---
+++ nameplate.rb
@@ -0,0 +1,29 @@
+
+require "rubygems"
+require "bundler"
+Bundler.setup
+
+require 'haml'
+require 'sinatra'
+require 'open-uri'
+require 'yajl'
+require 'rufus-json'
+
+get '/' do
+  redirect '/nameplate/jmettraux'
+end
+
+get '/nameplate/:twitterid' do
+
+  @id = params[:twitterid]
+
+  info = open("http://twitter.com/users/#{@id}.json").read rescue nil
+  info = Rufus::Json.decode(info)
+  #puts Rufus::Json.pretty_encode(info)
+
+  @name = info['name']
+  @image = info['profile_image_url']
+
+  haml :nameplate
+end
+
